4170c7de4065e453bf76e441481b6276ae4d9095
Joe Michael <jack_dexter@hotmail.com>
2025-02-22 18:19:01 -0300

    feat: doing first migration again

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,9 @@
-
+import * as migration_20250222_211811_first from './20250222_211811_first';
 
 export const migrations = [
-
+  {
+    up: migration_20250222_211811_first.up,
+    down: migration_20250222_211811_first.down,
+    name: '20250222_211811_first'
+  },
 ];
